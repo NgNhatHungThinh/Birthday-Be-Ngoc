fb99d157afda08c0428987c664d636e0e18bf621
NgNhatHungThinh <hungthinh2601@gmail.com>
2024-03-09 09:50:39 +0700

    09/03/24 - Release, Happy birthday to wife

--- script/index.js
+++ script/index.js
@@ -1,11 +1,5 @@
 var isOpen = false;
-var currentPhotoIndex = 1;
-
-// Phát nhạc khi trang web được tải
-var audio = document.getElementById("player");
-window.onload = function() {
-    audio.play();
-};
+var currentPhotoIndex = 1
 
 if(document.location.search.match(/type=embed/gi)) 
 {
@@ -15,15 +9,17 @@ if(document.location.search.match(/type=embed/gi))
 function showMessage() 
 {
     var coverBox = document.querySelector('.giftbox__cover');
+    var bodyBoxCap = document.getElementById('bodyCap'); 
 
     if(!isOpen) 
     {
         coverBox.classList.add('open');
+        bodyBoxCap.classList.remove('giftbox__capShadow'); 
         swal({
             title: "Chúc mừng sinh nhật em yêu ❤️️",
             text: "Cảm ơn em! Người đã mang đến cho anh niềm vui trong cuộc sống và vô vàn hứng khởi trong công việc. Nhân ngày sinh nhật em, anh chúc em luôn mạnh khỏe, ngày càng xinh đẹp và những mong muốn của em sớm thành hiện thực. Mong sao hai ta luôn bên nhau, cùng nhau trải qua nhiều dịp sinh nhật nữa nhé!. Happy Birthday, Bae!", 
             button: {
-                text: "❤️️",
+                text: "Cảm ơn anh ❤️️",
             },
         });
         isOpen = true;
@@ -31,6 +27,7 @@ function showMessage()
     else 
     {
         coverBox.classList.remove('open');
+        bodyBoxCap.classList.add('giftbox__capShadow'); 
         isOpen = false;
     }
 }
@@ -39,15 +36,14 @@ function nextPhoto()
 {
     var photos = document.querySelectorAll('.photo');
 
-    // Ẩn ảnh hiện tại
-    photos[currentPhotoIndex].style.display = 'none';
+    // Ẩn tất cả các ảnh trừ ảnh đầu tiên
+    for (let i = 1; i < photos.length; i++) {
+        photos[i].style.display = 'none';
+    }
 
-    // Tăng chỉ số ảnh hiện tại
-    currentPhotoIndex++;
+    // Chuyển sang ảnh tiếp theo
+    currentPhotoIndex = (currentPhotoIndex + 1) % photos.length; 
 
-    // Nếu đã đến ảnh cuối cùng, quay lại ảnh đầu tiên
-    if(currentPhotoIndex >= photos.length) currentPhotoIndex = 0; 
-
-    // Hiện ảnh tiếp theo
-    photos[currentPhotoIndex].style.display = 'block';
+    // Hiển thị ảnh tiếp theo
+    photos[currentPhotoIndex].style.display = 'block'; 
 }
